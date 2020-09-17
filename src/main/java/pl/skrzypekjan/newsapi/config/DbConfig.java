37c573b51232b1bb05f065de92ef4c6f0cb29d11
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-09-17 21:36:11 +0200

    Database connection configuration, creating sql table in the comment

--- src/main/java/pl/skrzypekjan/newsapi/config/DbConfig.java
+++ src/main/java/pl/skrzypekjan/newsapi/config/DbConfig.java
@@ -1,8 +1,10 @@
 package pl.skrzypekjan.newsapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -24,8 +26,8 @@ public class DbConfig {
 
 //    @EventListener(ApplicationReadyEvent.class)
 //    public void init() {
-//        String sql = "CREATE TABLE news(news_id int, title varchar(255), description varchar(255), url varchar(255)," +
-//                " urlImage varchar(255), publishedAt varchar(255), content varchar(255), PRIMARY KEY (news_id))";
+//        String sql = "CREATE TABLE news(news_id int, title varchar(255), description varchar(1000), url varchar(1000)," +
+//                " urlImage varchar(1000), publishedAt varchar(1000), content varchar(255), PRIMARY KEY (news_id))";
 //        getJdbcTemplate().update(sql);
 //    }
 }
